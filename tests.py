c9b37580f64f89d9333f9deb28f445464cdee7bd
soasme <soasme@gmail.com>
2016-02-18 10:50:06 +0800

    Test don't need to require real redis service.

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+import fakeredis
 
 from flask import Flask
 from flask.ext.cacheobj import FlaskCacheOBJ, Msgpackable
@@ -22,7 +23,8 @@ def cache(app, request):
     cache = FlaskCacheOBJ()
     app.config['CACHE_HOST'] = 'localhost'
     cache.init_app(app)
-    request.addfinalizer(cache.mc._flushall)
+    cache.mc = fakeredis.FakeStrictRedis()
+    request.addfinalizer(cache.mc.flushall)
     return cache
 
 def test_mc_initialized(cache):
