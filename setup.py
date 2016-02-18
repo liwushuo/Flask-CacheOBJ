49ba9a6ffb05a194e5ca3b7479dc2b034b54d083
soasme <soasme@gmail.com>
2016-02-18 16:35:47 +0800

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='Flask-CacheOBJ',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/liwushuo/Flask-CacheOBJ',
     license='MIT',
     author='Ju Lin',
